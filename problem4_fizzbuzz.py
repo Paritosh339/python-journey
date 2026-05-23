161531471a16d539054beb076e18873154455181
Paritosh Chakma <chakmaparitosh124@gmail.com>
2026-05-23 20:41:07 +0800

    Add problem4 fizzbuz

---
+++ problem4_fizzbuzz.py
@@ -0,0 +1,15 @@
+# Problem 4 - FizzBuzz
+# Print numbers from 1 to 50
+# Print "Fizz" for multiples of 3
+# Print "Buzz" for multiples of 5
+# Print "FizzBuzz" for multiples of both 3 and 5
+
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
