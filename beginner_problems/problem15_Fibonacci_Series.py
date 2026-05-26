5f9c19e365fb9d2c81874a66e2344fcd4cb712be
Paritosh Chakma <chakmaparitosh124@gmail.com>
2026-05-26 19:06:10 +0800

    Add problem15 Fibonacci series

---
+++ beginner_problems/problem15_Fibonacci_Series.py
@@ -0,0 +1,12 @@
+#Add problem15 fibonacci series
+
+number = int(input("Please enter your number: "))
+
+a = 0
+b = 1
+
+for i in range(number):
+    print(a, end=" ")
+    next = a + b
+    a = b
+    b = next
