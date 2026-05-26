ba6aae11cd70f261b9234f4a095f76ba67c172e0
Paritosh Chakma <chakmaparitosh124@gmail.com>
2026-05-26 14:50:31 +0800

    Fixed the file name

---
+++ Functions/10_function10_shopping_cart.py
@@ -0,0 +1,20 @@
+def shooping_cart(prices):
+    
+    total = 0
+
+    for price in prices:
+        total += price
+    
+    
+    if total > 1000 :
+        discount = (total * 0.1 )
+        final_price = (total - discount)
+        return(f"Total: {total}\nDiscount: {discount}\nFinal price: {final_price}")
+        
+    else:
+        return(f"Total: {total}\nDiscount: No discount applied\nFinal price: {total}")
+    
+prices = [500, 300, 400, 350]
+
+print(shooping_cart(prices))
+
